ede62551490aa75b35e733af04c2066771786ecc
npofsi | N/P硅 <npofsi@outlook.com>
2023-01-06 23:31:19 +0800

    Import tailwind CSS

--- components/lintlib/section_layout.js
+++ components/lintlib/section_layout.js
@@ -8,20 +8,6 @@ export default function Section_Layout({ children, corner }) {
       <section className={styles.section}>
         <div className={styles.container}>
           <div className={styles.content}>{children}</div>
-
-          <div
-            className={cn({
-              [styles.bgholder_left]: corner === "left",
-              [styles.bgholder_right]: corner === "right",
-            })}
-          >
-            <div
-              className={cn({
-                [styles.mask_left]: corner === "left",
-                [styles.mask_right]: corner === "right",
-              })}
-            ></div>
-          </div>
         </div>
       </section>
     </>
---
+++ components/navbar.js
@@ -0,0 +1,22 @@
+
+
+
+export default function Navbar() {
+  return (
+    <>
+      <div className="navbarContainer">
+        <h1>Hello World</h1>
+      </div>
+      <style jsx>
+        {`
+          .navbarContainer {
+            display: block;
+            position:fixed;
+            background-color:green;
+            width:100%;
+          }
+        `}
+      </style>
+    </>
+  )
+}
