43450aa58e540be28e3c151e882e20f58ad228bc
npofsi | N/P硅 <npofsi@outlook.com>
2023-01-15 21:28:00 +0800

    Fix fonts

--- pages/playground.js
+++ pages/playground.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Link from "next/link";
 import Layout from "../components/layout";
 import SectionMain from "../components/sectionMain";
@@ -6,16 +7,22 @@ export default function Playground({}) {
   return (
     <>
       <Layout navActiveIndex={3}>
+        <Head>
+          <title>Playground</title>
+        </Head>
         <SectionMain>
           <div className={styles.playgroundWrap}>
             <div className={styles.playgroundList}>
               <div className={styles.playgroundItem}>
                 <div className={styles.playgroundBgPadding}></div>
-                <div className={styles.playgroundContent}>
-                  <Link href={""}>
-                    <a className={styles.playgroundLink}>Example Project</a>
-                  </Link>
-                </div>
+                <Link href={"/"}>
+                  <div className={styles.playgroundContent}>
+                    <span className={styles.playgroundLink}>
+                      Example Project
+                    </span>
+                    <span className={styles.playgroundDesc}>Description</span>
+                  </div>
+                </Link>
               </div>
             </div>
           </div>
