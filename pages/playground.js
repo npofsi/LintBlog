097521f5c43a70205511bc41ff5c6594862aed5f
npofsi | N/P硅 <npofsi@outlook.com>
2025-04-14 16:58:02 +0800

    fix: page flex-direction error on playground

--- pages/playground.js
+++ pages/playground.js
@@ -13,29 +13,44 @@ export default function Playground({}) {
         <SectionMain>
           <div className={styles.playgroundWrap}>
             <div className={styles.playgroundList}>
+                
               <div className={styles.playgroundItem}>
-                <div className={styles.playgroundBgPadding} style={{background: "center url('/canvas-painter/preview.png')"}}></div>
+                <div
+                  className={styles.playgroundBgPadding}
+                  style={{
+                    background: "center url('/canvas-painter/preview.png')",
+                  }}
+                ></div>
                 <Link href={"/canvas-painter/index.html"}>
                   <div className={styles.playgroundContent}>
                     <span className={styles.playgroundLink}>
-                    chenyulian / canvas-painter
+                      chenyulian / canvas-painter
                     </span>
-                    <span className={styles.playgroundDesc}>一个支持 Stylus Web API 的画板</span>
+                    <span className={styles.playgroundDesc}>
+                      一个支持 Stylus Web API 的画板
+                    </span>
                   </div>
                 </Link>
               </div>
-            </div>
-            <div className={styles.playgroundItem}>
-                <div className={styles.playgroundBgPadding} style={{background: "center url('/sqooosh/preview.png')"}}></div>
+
+              <div className={styles.playgroundItem}>
+                <div
+                  className={styles.playgroundBgPadding}
+                  style={{ background: "center url('/sqooosh/preview.png')" }}
+                ></div>
                 <Link href={"/sqooosh/index.html"}>
                   <div className={styles.playgroundContent}>
                     <span className={styles.playgroundLink}>
-                    GoogleChromeLab / sqooosh
+                      GoogleChromeLab / sqooosh
                     </span>
-                    <span className={styles.playgroundDesc}>一个简单好用的图片压缩工具</span>
+                    <span className={styles.playgroundDesc}>
+                      一个简单好用的图片压缩工具
+                    </span>
                   </div>
                 </Link>
               </div>
+
+            </div>
           </div>
         </SectionMain>
       </Layout>
